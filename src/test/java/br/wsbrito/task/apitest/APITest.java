228e77e01a754bc62186836d67a11b4f3b227fd8
wsbrito <wsbrito1975@gmail.com>
2021-04-06 21:54:36 -0300

    My frist version of task-api-test project.

---
+++ src/test/java/br/wsbrito/task/apitest/APITest.java
@@ -0,0 +1,72 @@
+package br.wsbrito.task.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+//	public void test() {
+//		RestAssured
+//			.given()
+//				.log().all()
+//			.when()
+//				.get("http://localhost:8080/tasks-backend/todo")
+//			.then()
+//				.log().all()
+//			;
+//	}
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8080/tasks-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured
+			.given()
+				//.log().all()
+			.when()
+				.get("/todo")
+			.then()
+				//.log().all()
+				.statusCode(200)
+			;
+	}
+
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		RestAssured
+			.given()
+				.body("{ \"task\":\"Teste via API\",\"dueDate\":\"2021-04-07\"}")
+				.contentType(ContentType.JSON)
+				//.log().all()
+			.when()
+				.post("/todo")
+			.then()
+				//.log().all()
+				.statusCode(201)
+			;
+	}
+
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		RestAssured
+			.given()
+				.body("{ \"task\":\"Teste via API\",\"dueDate\":\"2011-04-07\"}")
+				.contentType(ContentType.JSON)
+				//.log().all()
+			.when()
+				.post("/todo")
+			.then()
+				.log().all()
+				.statusCode(400)
+				.body("message", CoreMatchers.is("Due date must not be in past"))
+			;
+	}
+
+}
